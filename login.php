227148347fad15de05738067ed73f11c825e1de8
Jorge Rojas Aragonés <jeragones@gmail.com>
2012-11-17 18:13:15 -0600

    Commit 25

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
     	$rows = mysql_num_rows($query);
     	if($row = mysql_fetch_array($query, $rows)) {
     		$_SESSION['usuario'] = $row['usuario'];
+            alert();
             if($row['permiso']==2) {
                 header('Location: administrar.php');
             } else {
