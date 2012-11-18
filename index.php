c80d99d1d75c1547d4bb64e27d4fe76afb9ee2a4
Fabrizzio Salazar Espinoza <fabri10.se@gmail.com>
2012-11-17 18:53:55 -0600

    Commit 27

--- index.php
+++ index.php
@@ -73,9 +73,9 @@
         </nav> 
         <div class="banner">
             <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="32" height="32">
-                <param name="movie" value="MangBruncBanner.swf" />
+                <param name="movie" value="Banner1.swf" />
                 <param name="quality" value="high" />
-                <embed src="MangBruncBanner.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="160" height="397"></embed>
+                <embed src="Banner1.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="160" height="397"></embed>
             </object>
         </div>               
         <article>   
